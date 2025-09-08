4f4342715c9efe4f2f661c9047579cc7b10e69d2
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2025-09-08 17:56:44 +0530

    feat: add production configuration for better-sqlite3 in knexfile

--- server/knexfile.js
+++ server/knexfile.js
@@ -9,4 +9,14 @@ module.exports = {
 		migrations: { directory: path.join(__dirname, "src", "db", "migrations") },
 		seeds: { directory: path.join(__dirname, "src", "db", "seeds") },
 	},
+	production: {
+		client: "better-sqlite3",
+		// Use env or default to disk mount
+		connection: {
+			filename: process.env.SQLITE_FILENAME || "/var/data/app.sqlite",
+		},
+		useNullAsDefault: true,
+		migrations: { directory: path.join(__dirname, "src", "db", "migrations") },
+		seeds: { directory: path.join(__dirname, "src", "db", "seeds") },
+	},
 };
