9d90d70d039b08c87a74a73919d0226ce8e68899
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2025-09-08 18:09:24 +0530

    refactor: remove production configuration for better-sqlite3 from knexfile

--- server/knexfile.js
+++ server/knexfile.js
@@ -9,14 +9,4 @@ module.exports = {
 		migrations: { directory: path.join(__dirname, "src", "db", "migrations") },
 		seeds: { directory: path.join(__dirname, "src", "db", "seeds") },
 	},
-	production: {
-		client: "better-sqlite3",
-		// Use env or default to disk mount
-		connection: {
-			filename: process.env.SQLITE_FILENAME || "/var/data/app.sqlite",
-		},
-		useNullAsDefault: true,
-		migrations: { directory: path.join(__dirname, "src", "db", "migrations") },
-		seeds: { directory: path.join(__dirname, "src", "db", "seeds") },
-	},
 };
